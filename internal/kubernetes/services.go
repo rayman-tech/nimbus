8fa61946ac63d373eda0352fbe3a27d12e25afc6
Prayuj Tuli <prayujtuli@hotmail.com>
2025-05-04 17:53:05 -0400

    refactor: updating service to map http traffic to svc port 80

--- internal/kubernetes/services.go
+++ internal/kubernetes/services.go
@@ -14,6 +14,7 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	intstr "k8s.io/apimachinery/pkg/util/intstr"
 )
 
 func GenerateServiceSpec(namespace string, newService *models.Service, oldService *database.Service) (*corev1.Service, error) {
@@ -62,8 +63,9 @@ func GenerateServiceSpec(namespace string, newService *models.Service, oldServic
 				// then a NodePort will be be randomly selected
 				// otherwise, will use this port as ClusterIP
 				spec.Ports = append(spec.Ports, corev1.ServicePort{
-					Name: fmt.Sprintf("port-%d", idx),
-					Port: port,
+					Name:       fmt.Sprintf("port-%d", idx),
+					Port:       port,
+					TargetPort: intstr.FromInt(80),
 				})
 			}
 		}
