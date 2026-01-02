e519b582d390e8ad5afe28572adca54e43283214
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2026-01-02 13:05:47 -0500

    chore: run formatter

--- schema.sql
+++ schema.sql
@@ -1,40 +1,38 @@
 CREATE TABLE projects (
-  id        UUID        PRIMARY KEY DEFAULT gen_random_uuid(),
-  name      TEXT        NOT NULL
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid (),
+  name text NOT NULL
 );
 
 CREATE TABLE services (
-  id                UUID        PRIMARY KEY DEFAULT gen_random_uuid(),
-  project_id        UUID        NOT NULL,
-  project_branch    TEXT        NOT NULL,
-  service_name      TEXT        NOT NULL,
-  node_ports        INTEGER[]   NULL,
-  ingress           TEXT        NULL,
-  FOREIGN KEY (project_id) REFERENCES projects(id)
-    ON DELETE CASCADE,
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid (),
+  project_id uuid NOT NULL,
+  project_branch text NOT NULL,
+  service_name text NOT NULL,
+  node_ports integer[] NULL,
+  ingress text NULL,
+  FOREIGN KEY (project_id) REFERENCES projects (id) ON DELETE CASCADE,
   UNIQUE (project_id, project_branch, service_name)
 );
 
 CREATE TABLE volumes (
-  identifier        UUID    PRIMARY KEY,
-  volume_name       TEXT    NOT NULL,
-  project_id        UUID    NOT NULL,
-  project_branch    TEXT    NOT NULL,
-  size              INTEGER NOT NULL,
-  FOREIGN KEY (project_id) REFERENCES projects(id)
-    ON DELETE CASCADE
+  identifier uuid PRIMARY KEY,
+  volume_name text NOT NULL,
+  project_id uuid NOT NULL,
+  project_branch text NOT NULL,
+  size integer NOT NULL,
+  FOREIGN KEY (project_id) REFERENCES projects (id) ON DELETE CASCADE
 );
 
 CREATE TABLE users (
-  id       UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  username TEXT NOT NULL,
-  api_key  TEXT NOT NULL
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid (),
+  username text NOT NULL,
+  api_key text NOT NULL
 );
 
 CREATE TABLE user_projects (
-  user_id    UUID NOT NULL,
-  project_id UUID NOT NULL,
+  user_id uuid NOT NULL,
+  project_id uuid NOT NULL,
   PRIMARY KEY (user_id, project_id),
-  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-  FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+  FOREIGN KEY (project_id) REFERENCES projects (id) ON DELETE CASCADE
 );
